64b2c6ad1d1cadc16cb15db63a70c5936949dbf1
goodmean <zizigoo12@gmail.com>
2022-08-02 13:44:50 +0900

    select

---
+++ members.sql
@@ -0,0 +1,65 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: market_db
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `members`
+--
+
+DROP TABLE IF EXISTS `members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `members` (
+  `mem_id` char(8) NOT NULL,
+  `mem_name` varchar(10) NOT NULL,
+  `mem_number` int(11) NOT NULL,
+  `addr` char(2) NOT NULL,
+  `phone1` char(3) DEFAULT NULL,
+  `phone2` char(8) DEFAULT NULL,
+  `height` smallint(6) DEFAULT NULL,
+  `debut_date` date DEFAULT NULL,
+  PRIMARY KEY (`mem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `members`
+--
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+INSERT INTO `members` VALUES ('APN','에이핑크',6,'경기','031','77777777',164,'2011-02-10'),('BLK','블랙핑크',4,'경남','055','22222222',163,'2016-08-08'),('GRL','소녀시대',8,'서울','02','44444444',168,'2007-08-02'),('ITZ','잇지',5,'경남',NULL,NULL,167,'2019-02-12'),('MMU','마마무',4,'전남','061','99999999',165,'2014-06-19'),('OMY','오마이걸',7,'서울',NULL,NULL,160,'2015-04-21'),('RED','레드벨벳',4,'경북','054','55555555',161,'2014-08-01'),('SPC','우주소녀',13,'서울','02','88888888',162,'2016-02-25'),('TWC','트와이스',9,'서울','02','11111111',167,'2015-10-19'),('WMN','여자친구',6,'경기','031','33333333',166,'2015-01-15');
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'market_db'
+--
+
+--
+-- Dumping routines for database 'market_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-02 13:43:25
